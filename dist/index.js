88415b41c191e2107afdc8c3ce808874947a60c2
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-07-22 11:37:35 +0300

    add onload

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
 import { buttonOrder, ingridientsContainer, orderList, ingr, Categories } from "./constants.js";
 import { createIngridientsList } from "./createElements.js";
 import { addItemToOrderList, deleteItemFromOrderList, orderButtonClickHandler } from './eventListener.js';
+window.onload = function startApp() {
+    renderIngridients();
+    ingridientsContainer.addEventListener('click', addItemToOrderList);
+    orderList.addEventListener('click', deleteItemFromOrderList);
+    buttonOrder.addEventListener('click', orderButtonClickHandler);
+};
 export function renderIngridients() {
     createIngridientsList(ingr.main, Categories.main);
     createIngridientsList(ingr.meat, Categories.meat);
     createIngridientsList(ingr.sauce, Categories.sauce);
     createIngridientsList(ingr.vegetables, Categories.vegetables);
 }
-renderIngridients();
-ingridientsContainer.addEventListener('click', addItemToOrderList);
-orderList.addEventListener('click', deleteItemFromOrderList);
-buttonOrder.addEventListener('click', orderButtonClickHandler);
